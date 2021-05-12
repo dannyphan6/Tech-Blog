061780d38f578e8ed46ffcf31bd5b95c3733007a
Daniel Phan <dannyphan@comcast.net>
2021-05-11 22:41:15 -0700

    Set up directories and added content to files

---
+++ controllers/api/commentRoute.js
@@ -0,0 +1,9 @@
+const { Comment } = require('../../models/Comment')
+const router = require('express').Router()
+const withAuth = require('../../utils/auth')
+
+router.get('/', withAuth, async (req, res) => {
+    // try {
+        
+    // }
+})
