9f2644ee2e06a134b88adbd2db87788034ec9c44
Daniel Phan <dannyphan@comcast.net>
2021-05-31 04:36:14 -0700

    Added logged in data to rendering methods for conditionals to function correctly

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -5,12 +5,16 @@ const withAuth = require('../utils/auth');
 // Get route to render dashboard handlebar
 router.get('/', withAuth, (req, res) => {
     console.log("hello world");
-    res.render('dashboard')
+    res.render('dashboard', {
+        logged_in: req.session.logged_in
+    })
 })
 
 // Get route to render newpost handlebar
 router.get('/newpost', withAuth, (req, res) => {
-    res.render('newpost')
+    res.render('newpost', {
+        logged_in: req.session.logged_in
+    })
 })
 
 module.exports = router
