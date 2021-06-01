edd22c17247dcba377a33a0691b3b08ad32edb4e
Daniel Phan <dannyphan@comcast.net>
2021-05-31 22:30:22 -0700

    Added code to send post data to dashboards handlebar

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -3,9 +3,22 @@ const { Comment, Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Get route to render dashboard handlebar
-router.get('/', withAuth, (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     console.log("hello world");
+    const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+    // Serialize data so the template can read it
+    const posts = postData.map((posts) => posts.get({ plain: true }));
+
     res.render('dashboard', {
+        posts,
         logged_in: req.session.logged_in
     })
 })
